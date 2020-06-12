a439b74b6720f6ab6a9f19add9201bfbd7c76d4e
Jyotishman Ghatak <53049054+Jyotishman-Ghatak@users.noreply.github.com>
2020-06-12 20:39:30 +0530

    Add main.py
    This python folder contains the backend of the web app. Flask micro framework have been used.

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
